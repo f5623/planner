b463c4f7783c31fe44dffead0d28630349300736
arv <aerofaesk@proton.me>
2023-04-22 16:20:07 -0600

    adding a for to ask user days

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import { useState } from 'react';
 import './App.css';
-
+import Planner from './planner'
 function App() {
+    const [daysNumber, setDaysNumber] = useState('');
+    const [weekStart, setWeekStart] = useState('');
+    const [formSubmitted, setFormSubmitted] = useState(false);
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      setFormSubmitted(true);
+    }
+  
+    if (formSubmitted) {
+      return <Planner daysNumber={daysNumber} weekStart={weekStart}/>;
+    }
   return (
     <div className="App">
-      
+      <form onSubmit={handleSubmit}>
+        <label>
+          Days Number:
+          <input
+            type="number"
+            value={daysNumber}
+            onChange={event => setDaysNumber(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Week Start:
+          <input
+            type="number"
+            value={weekStart}
+            onChange={event => setWeekStart(event.target.value)}
+          />  Notice : saturday is 0 and Friday is 7.
+        </label>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+
     </div>
   );
 }
